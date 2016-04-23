4bf2e308ad4568ededf9f1cbafe3d85827fce952
Fehrain <fehrain@gmail.com>
2016-04-22 21:45:52 -0500

    Transfer of files
    Transferring files into repository

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('div').click(function(){
+     
+	 var questions = new Array();
+	 
+	 questions [0] = "1. Who makes the Focus?";
+	 questions [1] = "2. Where did the Mini Cooper originate?";
+	 questions [2] = "3. Who makes the Camry?";
+	 
+	 var answers = new Array();
+	 
+	 answers [0] = new Array();
+	 answers [0][0] = "Ford";
+	 answers [0][1] = "Chevrolet";
+	 answers [0][2] = "GMC";
+	 answers [0][3] = "Pontiac";
+	 
+	 answers [1] = new Array();
+	 answers [1][0] = "USA";
+	 answers [1][1] = "United Kingdom";
+	 answers [1][2] = "Japan";
+	 answers [1][3] = "Germany";
+	 
+	 
+		
+		
+    });
+	/*var allQuestions = [{question: "Who makes the Focus?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0},
+		{question: "Who makes the Focus?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0}
+		{question: "Who makes the Focus?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0}
+		{question: "Who makes the Focus?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0}
+		{question: "Who makes the Focus?", choices: ["David Cameron", "Gordon Brown", "Winston Churchill", "Tony Blair"], correctAnswer:0}
+		];*/
+});
